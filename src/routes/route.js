2bdb99bef4096796d20a74ead885fce5abce0532
Sanhil143 <sanhilrai143@gmail.com>
2022-12-11 00:26:36 +0530

    api-assign

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,77 +1,97 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
 const _ = require('underscore')
-const mentorModule = require('../abc/xyz/myModule'); 
-const req = require('express/lib/request');
-const { route } = require('express/lib/application');
 
 
-router.get("/profile-details", function(req, res){
-    // Write the LOGIC here
-    res.send('dummy response')
-})
 
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
-});
+//Api assignment
 
 
-router.get('/test-me', function(req, res){
-    console.log("I am here")
-    res.send("any dummy text from route handler 2")
-})
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
 
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+router.get("/movies", function(req,res){
+    const moviesData = ['Herapheri','Golmaal','Dhol','Dhoom','Bahubali']
+    console.error("Movies");
+    res.send(moviesData)
     
-    res.send('Dummy response')
 })
 
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
+
+router.get("/movies/:indexNumber", function(req,res){
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+    if(req.params.indexNumber < 0 || req.params.indexNumber > movies.length-1){
+        res.send("Please type valid number")
+    }
+    else{
+        res.send(movies[req.params.indexNumber])
+    }
+    
+
 })
 
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
+router.get("/films", function(req,res){
+    
+    const moviesArr = [ {
+        id: 1,
+        name: 'The Shining'
+        }, {
+        id: 2,
+        name: 'Incendies'
+        }, {
+        id: 3,
+        name: 'Rang de Basanti'
+        }, {
+        id: 4,
+        name: 'Finding Nemo'
+        }]
+    res.send(moviesArr)
+    console.error("Films");
 })
+
+router.get("/films/:filmsId",function(req,res){
+    let moviesArr1 = [ {
+        id: 1,
+        name: 'The Shining'
+        }, {
+        id: 2,
+        name: 'Incendies'
+        }, {
+        id: 3,
+        name: 'Rang de Basanti'
+        }, {
+        id: 4,
+        name: 'Finding Nemo'
+        }]
+
+
+
+        let filmReq = req.params.filmsId
+        let temp 
+        
+
+        for(let i=0; i<moviesArr1.length; i++){
+           if(moviesArr1[i].id == filmReq){
+            temp = (moviesArr1[i].name)
+            
+        }
+        }
+
+        if(filmReq >= moviesArr1.length){
+            res.send('no movie exists with this id') 
+           
+        }
+        else{
+            res.send(temp)     
+        }
+        
+
+})
+
+
+
+
+
+
 
 module.exports = router;
